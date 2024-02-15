91c760c833a58f7f3e7fa50bd55acb886e2deb7a
Salman khan <sk9817375719@gmail.com>
2024-02-15 17:38:55 +0530

    Odd even Program

---
+++ Dev3/app.java
@@ -0,0 +1,11 @@
+package JAVA.Dev3;
+public class app{
+    public static void main(String[] args) {
+        int num=5;
+          if(num %2==0){
+            System.out.println("The number is even");
+          }else{
+            System.out.println("Number is odd");
+          }
+    }
+}
